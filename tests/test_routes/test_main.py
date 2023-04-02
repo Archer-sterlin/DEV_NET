f7f1d791c26345f19324b82845619e3c28ebc2b5
chile <wistler4@gmail.com>
2023-04-02 16:00:31 +0100

    bug fix

--- tests/test_routes/test_main.py
+++ tests/test_routes/test_main.py
@@ -1,6 +1,6 @@
 
 
 def test_user_create(test_client, user_payload):
-    response = test_client.post("/blog",json=user_payload)
+    response = test_client.get("/blog")
     assert response.status_code == 200
   
